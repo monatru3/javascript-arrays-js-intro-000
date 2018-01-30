2860dbfd2b7a71503aaf3e4ccf8f7a61907362d3
Simone D <monatru3@hotmail.com>
2018-01-30 01:35:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,11 +2,11 @@ var chocolateBars= ["snickers", "hundred grand", "kit kat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
 array[...element];
-return array;
+return array
 }
 
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
-  return array;
+  return array
 }
