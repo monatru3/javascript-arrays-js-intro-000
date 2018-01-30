e40b2c0d24af5bd33797c9c1f6da766196d5e2ca
Simone D <monatru3@hotmail.com>
2018-01-30 02:54:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,7 +16,9 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-return  array.push(element);
-
+  return array.push(element);
 
 }
+
+
+
