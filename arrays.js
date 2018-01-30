ef9f800ca12d95ba06b99006997c9b290c7628e4
Simone D <monatru3@hotmail.com>
2018-01-30 03:44:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,6 +40,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  var newArray=array.splice(0,array.length-1)
+  var newArray=array.slice(0,array.length-1)
   return newArray
 }
