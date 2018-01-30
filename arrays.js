4db505380cdd600dc1cedd4ab2b0cd4e35dfd0fe
Simone D <monatru3@hotmail.com>
2018-01-30 01:45:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars= ["snickers", "hundred grand", "kit kat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-var newarray=array.unshift(element);
+var newarray=newarray.unshift(element);
 return newarray
 }
 
